20efa6fb8dacca332b6efa527f25b40f90756851
BT_1N3_15 <BT_1N3_15@BT_1N3_15.bktech.local>
2018-02-15 08:28:23 -0500

    qwq

--- Lab 3.3/src/ControlStructures/MergeSort_PartitionSort.java
+++ Lab 3.3/src/ControlStructures/MergeSort_PartitionSort.java
@@ -2,17 +2,30 @@ package ControlStructures;
 
 import java.util.Arrays;
 
-public class MergeSort {
+public class MergeSort_PartitionSort {
 
 	public static void main(String[] args)
 	{
 		String[] a = {"d", "f", "a", "g", "c", "b", "e"};
 		for(String x : a)
+		{
+			System.out.print(x + ", ");
+		}
+		System.out.println();
+		long in = System.nanoTime();
+		String[] b = mergeSort(a);
+		long out = System.nanoTime();
+		long time = out - in;
+		System.out.println("mergeSort took " + time + " ns");
+		for(String x : b)
+		{
+			System.out.print(x + ", ");
+		}
 	}
 	
 	public static String[] mergeSort(String[] S)
 	{
-		if(S.length > 1)
+		if(S.length >= 2)
 		{
 			String[] s1 = new String[S.length/2];
 			s1 = Arrays.copyOfRange(S, 0, (S.length/2)-1);
@@ -32,7 +45,7 @@ public class MergeSort {
 		String[] combo = new String[x.length + y.length];
 		int xc = 0;
 		int yc = 0;
-		while(xc + yc < (x.length + y.length) )
+		while(xc + yc < (x.length + y.length) - 1)
 		{
 			if(x[xc].compareTo(y[yc])>=0)
 			{
@@ -48,12 +61,19 @@ public class MergeSort {
 		return combo;
 	}
 	
-	public static int partition(String[] x)
+	public static String[] partitionSort(String[] x, int a, int b)
+	{
+		int p = partition(x, a, b);
+		partitionSort(x)
+		
+	}
+	
+	public static int partition(String[] x, int a, int b)
 	{
 		String P = x[0];
 		int Piv = 0;
-		int L = 1;
-		int R = x.length;
+		int L = a;
+		int R = b;
 		while(R > L)
 		{
 			while(x[R].compareTo(P)<=0)
